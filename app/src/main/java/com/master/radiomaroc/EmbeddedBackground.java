f35ab40a4b6833ce5b29569334c3151ddf0db842
mouhssineerhoulii-collab <mouhssinee.rhoulii@gmail.com>
2026-09-06 23:02:10 +0100

    Prepare embedded selected background

---
+++ app/src/main/java/com/master/radiomaroc/EmbeddedBackground.java
@@ -0,0 +1,4 @@
+package com.master.radiomaroc;
+
+/** Embedded selected background data is injected at build time in future revisions. */
+final class EmbeddedBackground { private EmbeddedBackground() {} }
