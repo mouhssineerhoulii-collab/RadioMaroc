21c719d532ee3323ec2faf25298e731367cee001
mouhssineerhoulii-collab <mouhssinee.rhoulii@gmail.com>
2026-09-06 12:52:23 +0100

    Add station model

---
+++ app/src/main/java/com/master/radiomaroc/Station.java
@@ -0,0 +1,13 @@
+package com.master.radiomaroc;
+
+public class Station {
+    public final String name;
+    public final String subtitle;
+    public final String streamUrl;
+
+    public Station(String name, String subtitle, String streamUrl) {
+        this.name = name;
+        this.subtitle = subtitle;
+        this.streamUrl = streamUrl;
+    }
+}
