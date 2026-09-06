5c0470c665f419d20dd280177d86db7abfc2a5de
mouhssineerhoulii-collab <mouhssinee.rhoulii@gmail.com>
2026-09-06 13:02:53 +0100

    Upgrade station model for logos and multilingual metadata

--- app/src/main/java/com/master/radiomaroc/Station.java
+++ app/src/main/java/com/master/radiomaroc/Station.java
@@ -2,12 +2,25 @@ package com.master.radiomaroc;
 
 public class Station {
     public final String name;
-    public final String subtitle;
+    public final String subtitleAr;
+    public final String subtitleFr;
+    public final String subtitleEn;
     public final String streamUrl;
+    public final String logoUrl;
 
-    public Station(String name, String subtitle, String streamUrl) {
+    public Station(String name, String subtitleAr, String subtitleFr, String subtitleEn,
+                   String streamUrl, String logoUrl) {
         this.name = name;
-        this.subtitle = subtitle;
+        this.subtitleAr = subtitleAr;
+        this.subtitleFr = subtitleFr;
+        this.subtitleEn = subtitleEn;
         this.streamUrl = streamUrl;
+        this.logoUrl = logoUrl;
+    }
+
+    public String subtitle(String lang) {
+        if ("fr".equals(lang)) return subtitleFr;
+        if ("en".equals(lang)) return subtitleEn;
+        return subtitleAr;
     }
 }
